46870b53b63cfd431662b46b09408850e8fc349c
ganupalli sunitha <sunithaganupalli1999@gmail.com>
2023-11-06 10:52:00 +0530

    SECOND COMMIT

---
+++ in.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+ printf("sunitha first programme");
+ return 0;
+}
---
+++ variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    float f = 8.5;
+    char c = 's';
+    int b = 5;
+    printf("hi sunitha this is 2nd programme \n");
+    printf("the value of a is %d \n", a);
+    printf("the value of f is %f \n", f);
+    printf("the value of c is %c \n", c);
+    printf(" sum of a and b is %d \n", a + b);
+    return 0;
+}
