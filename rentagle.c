c1d59680ebbc1a52bfd9e1dca78e4aa1aa0c4c72
ganupalli sunitha <sunithaganupalli1999@gmail.com>
2023-11-06 12:22:56 +0530

    rentangle correct

--- rentagle.c
+++ rentagle.c
@@ -1,17 +1,1 @@
-//c programm with area of rentagle values
-#include<stdio.h>
-int a(int l, int b);
-int main()
-{
-    int 1;
-    printf("enter the value of 1:\n");
-    scanf("%d"&l);
-    int b;
-    printf("enter the value of bP:\n");
-    scanf("%d",&b);
-    printf("the area of rentangle is %d",(2*(l+b)));
-}
-int a(int l,int b)
-{
-    return (2*(l+b);)
-}
+s
